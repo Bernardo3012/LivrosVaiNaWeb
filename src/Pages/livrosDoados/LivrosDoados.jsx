1d0ce649d9deb6c42be19c08e5fa00d00b69cb74
Bernardo3012 <bernardo301204@gmail.com>
2024-11-29 10:34:51 -0300

    Adicionado pagina Livros Doados e o começo da responsividade

--- src/Pages/livrosDoados/LivrosDoados.jsx
+++ src/Pages/livrosDoados/LivrosDoados.jsx
@@ -1,7 +1,21 @@
+import livro1 from "../../assets/oProtagonista.png"
+import s from "./livrosDoados.module.scss"
+
 export default function LivrosDoados(){
     return(
-        <main>
-        
-        </main>
+        <section className={s.livrosDoadosSection}>
+            <h2>Livros Doados</h2>
+            <section className={s.cointanerCards}>
+                <section >
+                    <img src={livro1} alt="Livro de capa vermelha de nome O Protagonista." />
+                    <div>
+                        <h3>O protagonista</h3>
+                        <p>Susanne Andrade</p>
+                        <p>Ficção</p>
+                    </div>
+                    
+                </section>
+            </section>
+        </section>
     )
 }
